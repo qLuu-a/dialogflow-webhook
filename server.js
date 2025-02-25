e024ccf9f9a08f977d36d155e849d9d8d8a89a18
qLuu-a <alejandrolugo14_@hotmail.com>
2025-02-25 11:39:03 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+require('dotenv').config()
+
+console.log("Variables de entorno cargadas:", process.env);  // Ver todas las variables cargadas
+
 const express = require("express");
 const axios = require("axios");
 
@@ -35,5 +39,6 @@ app.post("/webhook", async (req, res) => {
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
+    console.log("API Key: ", DEEPSEEK_API_KEY);
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
